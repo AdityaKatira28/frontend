3b3f32155cc43ca3bf11dc5182a1a695c324bcbe
AdityaKatira28 <adityakatira@hotmail.com>
2025-06-19 20:34:25 +0530

    commit

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -7,7 +7,7 @@ import { ThreatDashboard } from '@/components/dashboard/ThreatDashboard';
 import BudgetOptimizationDashboard from '@/components/dashboard/BudgetOptimizationDashboard';
 import { ComplianceDashboard } from '@/components/dashboard/ComplianceDashboard';
 import { Toaster } from "@/components/ui/toaster";
-import { Sidebar } from '@/components/dashboard/Sidebar';
+import Sidebar from '@/components/dashboard/Sidebar';
 
 interface DashboardLayoutProps {
   children?: React.ReactNode;
